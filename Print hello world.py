77a1a42294c9f47132d21efccef8ae69e494d3b0
tanistar266 <tanistar266@gmail.com>
2025-06-17 13:10:27 +0530

    Add files via upload

---
+++ Print hello world.py
@@ -0,0 +1,5 @@
+# Intro Banner Project
+print("✨ Welcome to My Python World ✨")
+print("Name: Tanistar")
+print("Goal: Learn Python and earn money 💸")
+print("Favorite Quote: Distance, Silence, Clarity and Growth are Directly Proportional to each other.")
